3d31e3a4e24dd0ed2e38232b35044b561d5567b9
KingMike100 <nsubugamike021@gmail.com>
2021-03-29 22:15:36 +0300

    Added initial content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  showHintContent: Boolean = false;
+  showAnswerContent: Boolean = false;
+
+
+  showHint() {
+    this.showHintContent = this.showHintContent ? false : true;
+  }
+
+  showAnswer(){
+  this.showAnswerContent = this.showAnswerContent ? false : true;
+  }
 }
